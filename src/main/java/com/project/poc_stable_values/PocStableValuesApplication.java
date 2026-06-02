a4bdc4a02e71bc24df92e58f5f6f50e0a3693447
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-01 22:44:42 -0300

    feat: first commit

---
+++ src/main/java/com/project/poc_stable_values/PocStableValuesApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_stable_values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocStableValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocStableValuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_stable_values/PocStableValuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_stable_values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocStableValuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
