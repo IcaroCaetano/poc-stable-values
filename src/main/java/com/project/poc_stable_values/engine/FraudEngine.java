fc8ba94a44e3aca2b8d056107a607623801a67d1
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-02 22:54:51 -0300

    feat: add fraud engine

---
+++ src/main/java/com/project/poc_stable_values/engine/FraudEngine.java
@@ -0,0 +1,71 @@
+package com.project.poc_stable_values.engine;
+
+
+public class FraudEngine {
+
+    private final StableValue<FaceMatchModel> faceMatchModel =
+            StableValue.of(this::loadFaceMatchModel);
+
+    private final StableValue<LivenessModel> livenessModel =
+            StableValue.of(this::loadLivenessModel);
+
+    private final StableValue<JwtPublicKey> jwtPublicKey =
+            StableValue.of(this::loadJwtPublicKey);
+
+    public FaceMatchModel faceMatchModel() {
+        return faceMatchModel.get();
+    }
+
+    public LivenessModel livenessModel() {
+        return livenessModel.get();
+    }
+
+    public JwtPublicKey jwtPublicKey() {
+        return jwtPublicKey.get();
+    }
+
+    private FaceMatchModel loadFaceMatchModel() {
+
+        System.out.println(
+                "Loading Face Match Model..."
+        );
+
+        sleep();
+
+        return new FaceMatchModel("face-v3");
+    }
+
+    private LivenessModel loadLivenessModel() {
+
+        System.out.println(
+                "Loading Liveness Model..."
+        );
+
+        sleep();
+
+        return new LivenessModel("liveness-v2");
+    }
+
+    private JwtPublicKey loadJwtPublicKey() {
+
+        System.out.println(
+                "Loading JWT Public Key..."
+        );
+
+        sleep();
+
+        return new JwtPublicKey(
+                "kid-001",
+                "PUBLIC_KEY_CONTENT"
+        );
+    }
+
+    private void sleep() {
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
