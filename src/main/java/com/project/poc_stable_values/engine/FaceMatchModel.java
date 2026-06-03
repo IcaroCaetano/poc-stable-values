8569d0e2df53b0ed3df6751c15c6be9cc609d2bc
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-02 22:47:56 -0300

    feat: add dtos and engine class

--- src/main/java/com/project/poc_stable_values/engine/FaceMatchModel.java
+++ src/main/java/com/project/poc_stable_values/engine/FaceMatchModel.java
@@ -5,17 +5,17 @@ public class FaceMatchModel {
     private final String version;
 
     public FaceMatchModel(String version) {
+
         this.version = version;
     }
 
-    public double analyze(
-            String selfieBase64,
-            String documentBase64) {
+    public double analyze(String selfieBase64, String documentBase64) {
 
         return Math.random() * 100;
     }
 
     public String version() {
+
         return version;
     }
 }
