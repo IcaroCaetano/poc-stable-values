8e32e4f0ce5eb4fdb7837394f2be364819fa731a
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-09 22:23:41 -0300

    feat: update refactoring

--- src/main/java/com/project/poc_stable_values/service/FraudAnalysisService.java
+++ src/main/java/com/project/poc_stable_values/service/FraudAnalysisService.java
@@ -6,31 +6,20 @@ import com.project.poc_stable_values.engine.FraudEngine;
 
 public class FraudAnalysisService {
 
-    private final FraudEngine fraudEngine =
-            new FraudEngine();
+    private final FraudEngine fraudEngine = new FraudEngine();
 
-    public FraudAnalysis analyze(
-            FraudAnalysisRequest request) {
+    public FraudAnalysis analyze(FraudAnalysisRequest request) {
 
-        var faceMatchModel =
-                fraudEngine.faceMatchModel();
+        var faceMatchModel = fraudEngine.faceMatchModel();
 
-        var livenessModel =
-                fraudEngine.livenessModel();
+        var livenessModel = fraudEngine.livenessModel();
 
-        double faceMatchScore =
-                faceMatchModel.analyze(
+        double faceMatchScore = faceMatchModel.analyze(
                         request.selfieBase64(),
                         request.documentBase64());
 
-        double livenessScore =
-                livenessModel.analyze(
-                        request.selfieBase64());
+        double livenessScore = livenessModel.analyze(request.selfieBase64());
 
-        return new FraudAnalysis(
-                request.cpf(),
-                faceMatchScore,
-                livenessScore
-        );
+        return new FraudAnalysis(request.cpf(),faceMatchScore, livenessScore);
     }
 }
